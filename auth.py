6b9a01fae94f3cd07760a2d6e373c61f0c67ed8e
Anand Singh <42609735+Anand-S23@users.noreply.github.com>
2020-01-16 17:52:05 -0500

    storing user data in db instead of array

--- auth.py
+++ auth.py
@@ -1,50 +1,40 @@
-import bcrypt 
-from getpass import getpass
+import sqlite3
+import bcrypt
 
-all_users = []
+db = sqlite3.connect('auth.db')
+cursor = db.cursor()
 
-def create_user():
-    username = input('Username: ')
-    pass1 = getpass()
-    pass2 = getpass()
+# Only need to this to excute the first time (when there is no table)
+# cursor.execute('CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT)')
+# db.commit()
 
-    found = False
+def create_user(username, password, password_conf):
+  cursor.execute("""SELECT username FROM users WHERE username=?""", (username,))
+  exisiting_user = cursor.fetchone()
 
-    for i in all_users:
-        if i['username'] == username:
-            found = True
-            break
-    
-    if found:
-        print('User already exits. ')
+  if exisiting_user: 
+    print('User already exisits, login or choose another username.')
+
+  else:
+    if password == password_conf: 
+      encoded = password.encode('utf-8')
+      final_password = bcrypt.hashpw(encoded, bcrypt.gensalt())
+
+      cursor.execute("INSERT INTO users VALUES (?, ?)", (username, final_password))
+      db.commit()
+
+    else: 
+      print('Passwords did not match. ')
+
+def authenticate_user(username, password):
+  cursor.execute("""SELECT username, password FROM users WHERE username=?""", (username,))
+  user = cursor.fetchone()
+
+  if user: 
+    encoded_pass = password.encode('utf-8')
+    if bcrypt.checkpw(encoded_pass, user[1]):
+      print('You have logged on.')
     else:
-        if pass1 == pass2:
-            password = pass1.encode('utf-8')
-            hpass = bcrypt.hashpw(password, bcrypt.gensalt())
-
-            all_users.append({'username':username, 'password':hpass})
-        else: 
-            print('Passwords did not match. ')
-    
-    print(all_users)
-
-def authenticate():
-    username = input('Username: ')
-    password = getpass()
-
-    found = False
-
-    for c,i in enumerate(all_users):
-        if i['username'] == username:
-            found = True
-            location = c 
-            break
-        
-    if found: 
-        npass = password.encode('utf-8')
-        if bcrypt.checkpw(npass, all_users[location]['password']):
-            print('You have logged on.')
-        else:
-            print('Passwords don\'t match.')
-    else:
-        print('User not found.')
+      print('Passwords don\'t match.')
+  else:
+    print('User not found.')
